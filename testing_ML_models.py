740c49edb1ea997231644b5a703e1aab914df1b5
Allan <allanhender@gmail.com>
2025-09-21 19:52:06 +0100

    adding random forest

--- testing_ML_models.py
+++ testing_ML_models.py
@@ -1,4 +1,59 @@
 import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt
+from sklearn import tree
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
+
+
+
+
+
+
+
 
 dataframe = pd.read_csv("ML_ready_data.csv", index_col=0)
-print(dataframe.head())
+                        
+X = dataframe.drop(columns=["player1_win"])
+y = dataframe['player1_win']
+x_train, x_test, y_train, y_test = train_test_split(X,y, random_state=0, test_size=0.2)
+
+pipeline = Pipeline([("imputer",SimpleImputer(strategy='mean')), ('decision_tree', DecisionTreeClassifier(random_state=0))])
+
+pipeline.fit(x_train, y_train)
+y_pred = pipeline.predict(x_test)
+
+pipeline_score = pipeline.score(x_test, y_test)
+decision_tree_model = pipeline.named_steps['decision_tree']
+print(pipeline_score)
+
+
+
+rank_points = Pipeline([("imputer",SimpleImputer(strategy='mean'))])
+non_rank_points = Pipeline([("imputer",SimpleImputer(strategy='mean')), ("scale",StandardScaler())])
+
+
+rank_points_col = ["ranking_diff"]
+non_rank_cols = X.drop(columns=["ranking_diff"]).columns
+
+preprocess = ColumnTransformer(
+    transformers=[
+        ("rank_points_process", rank_points, rank_points_col),
+        ("non_rank_process", non_rank_points, non_rank_cols)
+    ]
+)
+
+logistic_regr_pipeline = Pipeline([("preprocess", preprocess), ("regr", LogisticRegression())])
+
+logistic_regr_pipeline.fit(x_train, y_train)
+y_pred = logistic_regr_pipeline.predict(x_test)
+
+
+#3. Calculate pipeline score and compare to estimator score
+#Pipeline score
+logistic_regr_pipeline_score = logistic_regr_pipeline.score(x_test, y_test)
+print(logistic_regr_pipeline_score)
